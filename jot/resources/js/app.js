97425949dcbe3b2464f0a3e60a9678f7d53b699c
thehanir95 <thehanicis95@gmail.com>
2021-07-20 18:05:27 +0530

    "Update"

--- jot/resources/js/app.js
+++ jot/resources/js/app.js
@@ -1,1 +1,18 @@
+import Vue from 'vue';
+import router from './router';
+import App from './components/app';
+
+
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+
+    router
+});
--- jot/routes/web.php
+++ jot/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/any', [App\Http\Controllers\HomeController::class, 'index'])->where('any','.*');
